f11850ddac1f430edde25041c86947ba9b73039e
Anton Wiklund <ixuz07@gmail.com>
2022-04-18 02:53:33 +0200

    feat: initial commit

---
+++ src/endpoints/TextEndpoint.ts
@@ -0,0 +1,21 @@
+import { IHttpHandler, IHttpHandlable } from '@antwika/http';
+
+export class TextEndpoint implements IHttpHandler {
+  private readonly code: number;
+  private readonly text: string;
+
+  constructor(code: number, text: string) {
+    this.code = code;
+    this.text = text;
+  }
+
+  async canHandle(handlable: IHttpHandlable) {
+    return true;
+  }
+
+  async handle(handlable: IHttpHandlable) {
+    const res = handlable.res();
+    res.writeHead(this.code);
+    res.end(this.text);
+  }
+}
---
+++ test/endpoints/TestEndpoint.test.ts
@@ -0,0 +1,19 @@
+import { TextEndpoint } from '../../src/endpoints/TextEndpoint';
+
+describe('TestEndpoint', () => {
+  it('can handle and respond to incoming requests', async () => {
+    const textEndpoint = new TextEndpoint(200, 'OK');
+
+    const req: any = { url: '/' };
+    const res: any = { 
+      writeHead: jest.fn(),
+      end: jest.fn(),
+    };
+    const handlable = { req: () => req, res: () => res };
+
+    expect(textEndpoint.canHandle(handlable)).resolves.toBeTruthy();
+    await textEndpoint.handle(handlable);
+    expect(res.writeHead).toHaveBeenCalledWith(200);
+    expect(res.end).toHaveBeenCalledWith('OK');
+  });
+});
